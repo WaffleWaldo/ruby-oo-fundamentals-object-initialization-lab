e30f760d4bd5d084984aec7eac1525e4b17344d3
WaffleWaldo <oswaldoortiz0519@gmail.com>
2020-09-14 20:38:12 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+    def initialize(name, breed = "Mutt")
+        @name = name
+        @breed = breed
+    end
+
+    def name_breed=(name, breed = "Mutt")
+        @name = name
+        @breed = breed
+    end
+
+    def name_breed
+        @name
+        @breed
+    end
+end
